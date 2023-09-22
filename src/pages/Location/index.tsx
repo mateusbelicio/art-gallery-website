8b3f7ed10140205b240bd264d698fe7db99335b6
Mateus Belicio <mateusbelicio@hotmail.com>
2023-09-22 17:54:12 -0300

    ✨: location page

--- src/pages/Location/index.tsx
+++ src/pages/Location/index.tsx
@@ -1,5 +1,17 @@
+import Map from '@/features/Map';
+import Footer from '@/ui/Footer';
+import OurLocation from '@/ui/OurLocation';
+
 function Location() {
-  return <div>LOCATION</div>;
+  return (
+    <>
+      <main>
+        <Map />
+        <OurLocation />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default Location;
